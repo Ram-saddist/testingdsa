042f3d007130771dcad0a202cb5b270f18def29c
Ram-saddist <>
2023-09-30 18:27:47 +0530

    version 4

--- src/app.js
+++ src/app.js
@@ -115,8 +115,6 @@ app.post("/booking", async (req, res) => {
     await newBook.save();
     res.json({ redirectTo: "/about?from=booking" });
   } catch (error) {
-    console.error("Error in signup:", error);
-    console.error("Attempted data:", data);
     req.flash("error", "An error occurred during signup. Please try again.");
     return null;
   }
@@ -171,15 +169,14 @@ app.post("/signup", async (req, res) => {
   };
   // Check if the username already exists in the database
   const existingUser = await collection.findOne({ email: data.email });
-  console.log("existing user", existingUser);
   if (existingUser) {
-    console.log("existing user function logic");
     // User already exists, redirect to signup page with a flash message
     req.flash(
       "error",
       "User already exists. Please choose a different username."
     );
-    res.redirect("/signup", { flashMessage: req.flash("error")[0] });
+    //res.redirect("/signup", { flashMessage: req.flash("error")[0] });
+    return res.redirect("/signup?from=exist")
   } else {
     // Hash the password using bcrypt
     const saltRounds = 10; // Number of salt rounds for bcrypt
@@ -193,20 +190,14 @@ app.post("/signup", async (req, res) => {
 
       // Create a new user document
       const newUser = new collection(data);
-      console.log("newuser in try block", newUser);
       await newUser.save();
 
       // Redirect to the login page
       return res.redirect("/login");
     } catch (error) {
-      console.error("Error in signup:", error);
-      console.error("Attempted data:", data);
       req.flash("error", "An error occurred during signup. Please try again.");
       return res.redirect("/signup");
     }
-
-    // Redirect to the login page
-    return res.redirect("/login");
   }
 });
 
@@ -248,7 +239,7 @@ app.post(
 app.get("/logout", (req, res) => {
   req.logout((err) => {
     if (err) {
-      console.error("Error logging out:", err);
+  
     }
     res.redirect("/login?from=logout"); // Redirect to the login page after logging out
   });
@@ -259,9 +250,7 @@ const otp = Math.floor(100000 + Math.random() * 900000);
 app.post("/send-email", async (req, res) => {
   try {
     // Generate a random OTP (e.g., a 6-digit number)
-
     const email = req.body.email;
-
     const transporter = nodemailer.createTransport({
       service: "gmail",
       auth: {
@@ -273,14 +262,50 @@ app.post("/send-email", async (req, res) => {
     const mailOptions = {
       from: "raghuveer@codegnan.com", // Sender address
       to: `${email}`, // Recipient address
-      subject: "Test Email from Node.js", // Subject line
+      subject: "Registration Confirmation", // Subject line
       html: `
-          <html>
-            <body>
-              <h1>Hello, John Doe</h1>
-              <p>Your OTP is: <strong>${otp}</strong></p>
-            </body>
-          </html>
+      <!DOCTYPE html>
+      <html lang="en">
+      <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>Registration Confirmation</title>
+          <style>
+              body {
+                  font-family: Arial, sans-serif;
+                  margin: 0 auto;
+                  max-width: 600px;
+                  padding: 20px;
+                  background-color: rgba(206, 238, 255, 0.5);
+              }
+              h1 {
+                  text-align: center;
+              }
+              img {
+                  display: block;
+                  margin: 0 auto;
+                  max-width: 100%;
+              }
+              .otp {
+                  color: #323596;
+                  font-weight: bold;
+                  font-size: 30px;
+              }
+          </style>
+      </head>
+      <body>
+          <!-- <img src="https://i.ytimg.com/vi/wq13sUIMWB0/maxresdefault.jpg" width="40%" /> -->
+          <h1>Welcome to Doctors Sports Academy</h1>
+          <p>Dear {name},</p>
+          <p>Greetings from Doctors Sports Academy We are thrilled to have received your
+              registration. Get ready to showcase your sportsmanship and camaraderie on the field!
+          </p>
+          <p>Your One-Time Password (OTP) for registration is:<span class="otp"><strong> ${otp} </strong></span></p>
+          
+          <p>Warm regards,</p>
+          <p>Doctors Sports Academy</p>
+      </body>
+      </html>
         `,
     };
 
@@ -290,7 +315,6 @@ app.post("/send-email", async (req, res) => {
     // Send the generated OTP back to the frontend as JSON
     res.status(200).json({ generatedOTP: otp });
   } catch (error) {
-    console.error("Error sending email:", error);
     res.status(500).send("An error occurred while sending the email.");
   }
 });
@@ -310,15 +334,26 @@ app.post("/validate-otp", (req, res) => {
 
 /* */
 app.get("/forgot-password", (req, res) => {
-  res.render("forgot-password");
+  console.log("req.query.from",req.query.from)
+  if(req.query.from==="invalidemail"){
+    console.log("invalid")
+    res.render("forgot-password",{
+      invalid: "User with this email not found",
+    })
+  }
+  else{
+    res.render("forgot-password",{
+      invalid: null,
+    });
+  }
+  
 });
 
 app.post("/forgot-password", async (req, res) => {
   const { email } = req.body;
   const user = await collection.findOne({ email: email });
-  console.log("from forget password route", email, user);
   if (!user) {
-    res.status(404).send("User with this email not found");
+   return res.redirect("/forgot-password?from=invalidemail")
   }
 
   const { otp, fullHash } = generateOtp(email);
@@ -326,17 +361,47 @@ app.post("/forgot-password", async (req, res) => {
   try {
     const subject = "Reset password";
     const text = `
-                  <html>
-                    <body>
-                      <h1>Hello, ${user.email}</h1>
-                      <p>Your OTP is: <strong>${otp}</strong></p>
-                    </body>
-                  </html>`;
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Password Reset</title>
+        <style>
+            body {
+                font-family: Arial, sans-serif;
+                margin: 0 auto;
+                max-width: 600px;
+                padding: 20px;
+                background-color: #f4f4f4;
+            }
+            h1 {
+                text-align: center;
+                color: #333;
+            }
+            p {
+                color: #555;
+            }  
+        </style>
+    </head>
+    <body>
+        <!-- Replace the image URL below with your own image URL -->
+        <!-- <div class="image-container">
+            <img src="https://i.ytimg.com/vi/wq13sUIMWB0/maxresdefault.jpg" width="40%" alt="Password Reset Image" />
+        </div> -->
+        <h1>Password Reset</h1>
+        <p>Hello,</p>
+        <p>We received a request to reset your password. Please click on the link below to reset your password:</p>
+        <p>Your OTP is: <strong>${otp}</strong></p>
+        <p>Best regards,</p>
+        <p>Your Support Team</p>
+    </body>
+    </html>              
+    `;
 
     const msg = { from: "max@arleven.com", to: email, subject, text };
     await transport.sendMail(msg);
   } catch (error) {
-    console.error(error);
   }
 
   Object.assign(user, {
@@ -377,11 +442,6 @@ const transport = nodemailer.createTransport({
 transport
   .verify()
   .then(() => console.log("Connected to email server"))
-  .catch(() =>
-    console.error(
-      "Unable to connect to email server. Make sure you have configured the SMTP options right"
-    )
-  );
 
 const aVeryLongSafeString =
   "874410ea46e7a92edcffd67911d1819c3d36ab93f00cc3de6f05ade3083a89a316f8d3207d86518f72f845a2ff771a92b98ef6fb233e48391b654240219b45ec";
@@ -392,7 +452,6 @@ const generateOtpHash = (email, otp, expires) => {
 };
 
 const verifyOtp = (hash, email, otp) => {
-  console.log("from verify otp", hash);
   const [hashValue, expires] = hash.split(".");
   const now = Date.now();
 
@@ -405,11 +464,6 @@ const verifyOtp = (hash, email, otp) => {
   const expiresAt = Number(expires);
 
   const newCalculatedHash = generateOtpHash(email, otp, expiresAt);
-  console.log(
-    "from verify otp function hashvalue and newcalculated",
-    newCalculatedHash,
-    hashValue
-  );
   if (newCalculatedHash !== hashValue) {
     return {
       error: "Incorrect OTP",
